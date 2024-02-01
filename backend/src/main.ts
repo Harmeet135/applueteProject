dd0431b1622a80a3fda180e7bcc0ff55c1d92c55
Harmeet Singh <harmeetchahal9891@gmail.com>
2024-02-01 19:41:11 +0530

    .

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import * as process from "process";
 config();
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
